8b1128bf66b08405ce7c997aea547c75d12dda7b
施颖杰 <1577310596@qq.com>
2022-07-20 22:09:10 +0800

    feat: add medium case

---
+++ questions/003-medium-count/test-cases.spec.ts
@@ -0,0 +1,9 @@
+import count from "./template";
+
+const arr = [1, 2, 3, 4, 5, 6]
+describe('first: count only ', () => {
+  expect(count(arr, 1)).toBe(1)
+  expect(count(arr, 2)).toBe(1)
+  expect(count(arr, 3)).toBe(1)
+  expect(count(arr, 4)).toBe(1)
+});
