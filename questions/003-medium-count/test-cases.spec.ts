202c90b9b111970ae90b1073988beec1a4018756
施颖杰 <1577310596@qq.com>
2022-07-21 23:52:43 +0800

    feat: finish first ref test-cases

--- questions/003-medium-count/test-cases.spec.ts
+++ questions/003-medium-count/test-cases.spec.ts
@@ -2,7 +2,7 @@ import count from "./template";
 
 const arr = [1, 2, 3, 4, 5, 6]
 describe('first: count only ', () => {
-  it('a + b = c', () => {
+  it.skip('a + b = c', () => {
     expect(count(arr, 1)).toBe(1)
     expect(count(arr, 2)).toBe(1)
     expect(count(arr, 3)).toBe(1)
