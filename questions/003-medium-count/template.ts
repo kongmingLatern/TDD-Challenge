da23b0ad1c5f075d669f576ed847b3fcacd046f5
施颖杰 <1577310596@qq.com>
2022-07-21 16:50:10 +0800

    change files

--- questions/003-medium-count/template.ts
+++ questions/003-medium-count/template.ts
@@ -1,4 +1,5 @@
 export default function count(arr: number[], num: number) {
   // write your code here
+  111
   return arr.reduce((prev, cur) => cur === num ? prev + cur : prev)
 };
--- questions/003-medium-count/test-cases.spec.ts
+++ questions/003-medium-count/test-cases.spec.ts
@@ -2,8 +2,10 @@ import count from "./template";
 
 const arr = [1, 2, 3, 4, 5, 6]
 describe('first: count only ', () => {
-  expect(count(arr, 1)).toBe(1)
-  expect(count(arr, 2)).toBe(1)
-  expect(count(arr, 3)).toBe(1)
-  expect(count(arr, 4)).toBe(1)
+  it('a + b = c', () => {
+    expect(count(arr, 1)).toBe(1)
+    expect(count(arr, 2)).toBe(1)
+    expect(count(arr, 3)).toBe(1)
+    expect(count(arr, 4)).toBe(1)
+  });
 });
