a1375ea12600241d633f959233c8b4a8f3c532b3
施颖杰 <1577310596@qq.com>
2022-07-27 23:19:00 +0800

    feat: add Medium Object Test

--- learn/Medium Object Test/util.test.ts
+++ learn/Medium Object Test/util.test.ts
@@ -1,11 +1,12 @@
 import Util from "./util";
 
-let util = null
+let util: Util | null = null
 
 beforeAll(() => {
   util = new Util()
 })
 
 test('test util.a function', () => {
+  // expect(util.a(1, 2)).toBe('12')
   // 
 });
