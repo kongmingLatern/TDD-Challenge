39dab576db2b31b7c8e48499a541ac633b5588e9
施颖杰 <1577310596@qq.com>
2022-07-27 22:59:58 +0800

    remove files

--- learn/Day1-Function/add.test.ts
+++
@@ -1,9 +0,0 @@
-import add from ".";
-
-describe('Function Add', () => {
-  it('1 + 1 should be 2', () => {
-    const a: number = 1
-    const b: number = 1
-    expect(add(a, b)).toBe(2);
-  });
-});
