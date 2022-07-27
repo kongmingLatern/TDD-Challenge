6c70ba1be43f93bd5b103e3048888f2ee108163f
施颖杰 <1577310596@qq.com>
2022-07-27 23:24:05 +0800

    rename files

---
+++ learn/Medium Object Test/demoFunction.test.ts
@@ -0,0 +1,19 @@
+// jest.mock 发现 util 是一个类，会自动把类的构造函数和方法变成 jest.fn()
+/**
+ * 相当于
+ * const Util = jest.fn()
+ * util.a = jest.fn()
+ * util.b = jest.fn() 
+ */
+jest.mock('./util')
+
+import Util from "./util"
+import demoFunction from "./demoFunction"
+
+test('test demoFunction', () => {
+  demoFunction(1, 2)
+  console.log(Util);
+  expect(Util).toHaveBeenCalled()
+  expect(Util.mock.instances[0].a).toHaveBeenCalled()
+  expect(Util.mock.instances[0].b).toHaveBeenCalled()
+});
---
+++ learn/Medium Object Test/demoFunction.ts
@@ -0,0 +1,10 @@
+import Util from "./util";
+
+const demoFunction = (a, b) => {
+  const util = new Util()
+  util.a(a)
+  util.b(b)
+}
+
+
+export default demoFunction;
