e4501cf06721fb0443a5e70a286e2d0993498a3e
施颖杰 <1577310596@qq.com>
2022-07-27 22:47:40 +0800

    feat: change var

--- learn/Day1-Function/add.test.ts
+++ learn/Day1-Function/add.test.ts
@@ -2,6 +2,8 @@ import add from ".";
 
 describe('Function Add', () => {
   it('1 + 1 should be 2', () => {
-    expect(add(1, 1)).toBe(2);
+    const a: number = 1
+    const b: number = 1
+    expect(add(a, b)).toBe(2);
   });
 });
