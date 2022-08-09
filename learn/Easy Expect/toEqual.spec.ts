d31577861cd75b76d3bd7b79130db90a58f35a4b
凤之兮原 <1577310596@qq.com>
2022-08-09 21:36:22 +0800

    chore: add toEqual expect

---
+++ learn/Easy Expect/toEqual.spec.ts
@@ -0,0 +1,17 @@
+const can1 = {
+  flavor: 'grapefruit',
+  ounces: 12,
+};
+const can2 = {
+  flavor: 'grapefruit',
+  ounces: 12,
+};
+
+describe('the La Croix cans on my desk', () => {
+  test('have all the same properties', () => {
+    expect(can1).toEqual(can2);
+  });
+  test('are not the exact same can', () => {
+    expect(can1).not.toBe(can2);
+  });
+});
