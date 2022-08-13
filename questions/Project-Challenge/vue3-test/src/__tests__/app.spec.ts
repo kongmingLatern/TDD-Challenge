d8511042900414a64fdb03974e1ea9a220af21b5
凤之兮原 <1577310596@qq.com>
2022-08-13 20:41:16 +0800

    chore: add vue@utils

--- questions/Project-Challenge/vue3-test/src/__tests__/app.spec.ts
+++ questions/Project-Challenge/vue3-test/src/__tests__/app.spec.ts
@@ -1,6 +1,10 @@
+import { shallowMount } from '@vue/test-utils'
 import { describe, it } from 'vitest';
+import App from '../App.vue'
 describe('My First Test', () => {
   it('App.vue should have text "app"', () => {
+    const wrapper = shallowMount(App)
+    console.log(wrapper);
 
   })
 
